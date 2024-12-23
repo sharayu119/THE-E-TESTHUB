c34010d2637a6de6d5501b2443d2e0cc5028b185
anodicpassion <pratikpawarkarad@gmail.com>
2024-12-23 13:06:44 +0530

    first commit

---
+++ loginapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class MyUser(User):
+    
+    imagepath=models.CharField(max_length=100,primary_key=True)
+
+    class Meta:
+        db_table='yadav'
